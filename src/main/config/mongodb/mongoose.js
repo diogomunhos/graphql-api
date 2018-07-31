cffd03587badd4564110327abd606e5424929b4c
Diogo Munhos <diogo.munhos@sulamerica.com.br>
2018-07-31 18:26:04 -0300

    ajustando db

--- src/main/config/mongodb/mongoose.js
+++ src/main/config/mongodb/mongoose.js
@@ -2,7 +2,7 @@ const config = require('./config');
 const mongoose = require('mongoose');
 module.exports = (database_name) => {
     mongoose.Promise = global.Promise;
-    const db_config = (process.env.NODE_ENV === "production" && database_name !== undefined) ? config.production.db : config.test.db;
+    const db_config = (process.env.NODE_ENV === "production" && database_name === undefined) ? config.production.db : config.test.db;
     const db = mongoose.connect(db_config);
     mongoose.connection.on('error', (err) => {
         console.log('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
